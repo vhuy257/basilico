284060c50a84364627a08cb2cf7e0084059f0b61
vhuy2571990@gmail.com <ldcc.vinhhuy>
2024-01-29 16:16:06 +0700

    remove optmize image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/basilico",
-    output: "export"    
+    output: "export",
+    images: {
+        unoptimized: true
+    }    
 };
 
 export default nextConfig;
