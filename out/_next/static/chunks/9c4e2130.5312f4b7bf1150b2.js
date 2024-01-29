8d657663d25defcdd512ff4c186d617a6ee4f3f9
vhuy2571990@gmail.com <ldcc.vinhhuy>
2024-01-29 15:58:45 +0700

    push out folder to deploy

---
+++ out/_next/static/chunks/9c4e2130.5312f4b7bf1150b2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[706],{4932:function(t,n,u){u.d(n,{T8x:function(){return c}});var a=u(3118);function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M496 203.3H312.36L256 32l-56.36 171.3H16l150.21 105.4-58.5 171.3L256 373.84 404.29 480l-58.61-171.3z"},child:[]}]})(t)}}}]);
